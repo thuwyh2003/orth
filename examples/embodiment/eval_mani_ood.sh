569019eada25ed137a3b12fb34fd759aca3df2e0
thuwyh2003 <w-yh22@mails.tsinghua.edu.cn>
2026-06-14 19:34:42 +0800

    wyh

--- examples/embodiment/eval_mani_ood.sh
+++ examples/embodiment/eval_mani_ood.sh
@@ -7,18 +7,22 @@ export SRC_FILE="${EMBODIED_PATH}/eval_embodied_agent.py"
 
 export HYDRA_FULL_ERROR=1
 
-EVAL_NAME=maniskill_ppo_openpi_pi05_orth_3
-CKPT_PATH=/home/wyh/RLinf/logs/20260428-15:29:52-maniskill_ppo_openpi_pi05_orth/maniskill_ppo_openpi_pi05_baseline/checkpoints/global_step_200/actor/model_state_dict/full_weights.pt    # Optional: .pt file or None, if None, will use the checkpoint in rollout.model.model_path
+EVAL_NAME=maniskill_ppo_openpi_pi05_orth_8_fullenv
+CKPT_PATH=/home/wyh/RLinf/logs/20260506-15:29:24-maniskill_ppo_openpi_pi05_orth/maniskill_ppo_openpi_pi05_baseline/checkpoints/global_step_280/actor/model_state_dict/full_weights.pt
+# /home/wyh/RLinf/logs/20260428-15:29:52-maniskill_ppo_openpi_pi05_orth/maniskill_ppo_openpi_pi05_baseline/checkpoints/global_step_200/actor/model_state_dict/full_weights.pt    # Optional: .pt file or None, if None, will use the checkpoint in rollout.model.model_path
 CONFIG_NAME=maniskill_ppo_openpi_pi05_orth_eval        # env.eval must be maniskill_ood_template
-TOTAL_NUM_ENVS=24 # total number of evaluation environments
-EVAL_ROLLOUT_EPOCH=8 # eval rollout epoch, total_trajectory_num = eval_rollout_epoch * total_num_envs
+TOTAL_NUM_ENVS=48 # total number of evaluation environments
+EVAL_ROLLOUT_EPOCH=2 # eval rollout epoch, total_trajectory_num = eval_rollout_epoch * total_num_envs
 
+# for env_id in \
+#     "PutOnPlateInScene25EEPose-v1"  "PutOnPlateInScene25MultiPlate-v1" "PutOnPlateInScene25VisionImage-v1" "PutOnPlateInScene25VisionTexture03-v1"\
+#      "PutOnPlateInScene25VisionTexture05-v1" \
+#     "PutOnPlateInScene25VisionWhole03-v1"  "PutOnPlateInScene25VisionWhole05-v1" \
+#     "PutOnPlateInScene25Carrot-v1" "PutOnPlateInScene25Plate-v1" "PutOnPlateInScene25Instruct-v1" \
+#     "PutOnPlateInScene25MultiCarrot-v1"  \
+#     "PutOnPlateInScene25Position-v1" "PutOnPlateInScene25PositionChangeTo-v1" ; \
 for env_id in \
-    "PutOnPlateInScene25VisionImage-v1" "PutOnPlateInScene25VisionTexture03-v1" "PutOnPlateInScene25VisionTexture05-v1" \
-    "PutOnPlateInScene25VisionWhole03-v1"  "PutOnPlateInScene25VisionWhole05-v1" \
-    "PutOnPlateInScene25Carrot-v1" "PutOnPlateInScene25Plate-v1" "PutOnPlateInScene25Instruct-v1" \
-    "PutOnPlateInScene25MultiCarrot-v1" "PutOnPlateInScene25MultiPlate-v1" \
-    "PutOnPlateInScene25Position-v1" "PutOnPlateInScene25EEPose-v1" "PutOnPlateInScene25PositionChangeTo-v1" ; \
+    "PutOnPlateInScene25Plate-v1"  ; \
 do
     obj_set="test"
     LOG_DIR="${REPO_PATH}/logs/eval/${EVAL_NAME}/$(date +'%Y%m%d-%H:%M:%S')-${env_id}-${obj_set}"
@@ -37,22 +41,22 @@ do
     ${CMD} 2>&1 | tee -a ${MEGA_LOG_FILE}
 done
 
-for env_id in \
-    "PutOnPlateInScene25Carrot-v1" "PutOnPlateInScene25MultiCarrot-v1" \
-    "PutOnPlateInScene25MultiPlate-v1" ; \
-do
-    obj_set="train"
-    LOG_DIR="${REPO_PATH}/logs/eval/${EVAL_NAME}/$(date +'%Y%m%d-%H:%M:%S')-${env_id}-${obj_set}"
-    MEGA_LOG_FILE="${LOG_DIR}/run_ppo.log"
-    mkdir -p "${LOG_DIR}"
-    CMD="python ${SRC_FILE} --config-path ${EMBODIED_PATH}/config/ \
-        --config-name ${CONFIG_NAME} \
-        runner.logger.log_path=${LOG_DIR} \
-        algorithm.eval_rollout_epoch=${EVAL_ROLLOUT_EPOCH} \
-        env.eval.total_num_envs=${TOTAL_NUM_ENVS} \
-        env.eval.init_params.id=${env_id} \
-        env.eval.init_params.obj_set=${obj_set} \
-        runner.ckpt_path=${CKPT_PATH}"
-    echo ${CMD}  > ${MEGA_LOG_FILE}
-    ${CMD} 2>&1 | tee -a ${MEGA_LOG_FILE}
-done
+# for env_id in \
+#     "PutOnPlateInScene25Carrot-v1" "PutOnPlateInScene25MultiCarrot-v1" \
+#     "PutOnPlateInScene25MultiPlate-v1" ; \
+# do
+#     obj_set="train"
+#     LOG_DIR="${REPO_PATH}/logs/eval/${EVAL_NAME}/$(date +'%Y%m%d-%H:%M:%S')-${env_id}-${obj_set}"
+#     MEGA_LOG_FILE="${LOG_DIR}/run_ppo.log"
+#     mkdir -p "${LOG_DIR}"
+#     CMD="python ${SRC_FILE} --config-path ${EMBODIED_PATH}/config/ \
+#         --config-name ${CONFIG_NAME} \
+#         runner.logger.log_path=${LOG_DIR} \
+#         algorithm.eval_rollout_epoch=${EVAL_ROLLOUT_EPOCH} \
+#         env.eval.total_num_envs=${TOTAL_NUM_ENVS} \
+#         env.eval.init_params.id=${env_id} \
+#         env.eval.init_params.obj_set=${obj_set} \
+#         runner.ckpt_path=${CKPT_PATH}"
+#     echo ${CMD}  > ${MEGA_LOG_FILE}
+#     ${CMD} 2>&1 | tee -a ${MEGA_LOG_FILE}
+# done
